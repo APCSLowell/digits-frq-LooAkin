45025ecf6a6bda9d25e573ed8d5e34c44331f62f
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-04-14 16:46:22 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,19 +8,19 @@ public class Digits
 	public Digits(int num)
 	{ 
 		int listSize = 0;
+		ArrayList <Integer> digitList; 
 		for(int i = num; i >= 10; i=i/10)
 			listSize++;
-		int [] digitList = new int [listSize];
 		for(int j = listSize-1; j >= 0; j--){
-			digitList[j] = (num % 10);
+			digitList.add(0,(num % 10));
 			num = num/10;
 		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ 
-		for(int k = 0; k < digitList.length()-1; k++){
-			if((digitList[k]) >= (digitList[k+1]))
+		for(int k = 0; k < digitList.size()-1; k++){
+			if(digitList.get(k) >= digitList.get(k+1))
 				return false;
 		}
 		return true;
