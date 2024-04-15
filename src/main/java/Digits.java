39ebb1daa1f8464108858f7dddf79f38cdf8fddb
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-04-15 12:29:33 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,7 +8,7 @@ public class Digits
 	public Digits(int num)
 	{ 
 		int listSize = 0;
-		for(int i = num; i >= 10; i=i/10)
+		for(int i = num; i >= 0; i=i/10)
 			listSize++;
 		for(int j = listSize-1; j >= 0; j--){
 			digitList.add(0, new Integer(num % 10));
